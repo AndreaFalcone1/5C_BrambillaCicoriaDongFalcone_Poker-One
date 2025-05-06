fbc0191691649bf4d4131a4881877a534cbb016d
Andrea Falcone <157415531+AndreaFalcone1@users.noreply.github.com>
2025-05-06 18:38:43 +0200

    Inizio Register

---
+++ backend/components/register/createRegisterHandler.js
@@ -0,0 +1,14 @@
+export const createRegisterHandler = function(socket) {
+
+    return {
+        registerReciver: async  function () {
+            await socket.on('registerSender', (message) => {
+                
+            })
+        },
+        registerSender: async  function (data) {
+            await socket.emit('registerReciver', data);
+        }
+    }
+
+}
---
+++ backend/server.js
@@ -0,0 +1,53 @@
+//
+//  Libraries
+//
+
+const fs = require('fs');
+const express = require("express");
+const http = require("http");
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const { Server } = require('socket.io'); 
+const conf = JSON.parse(fs.readFileSync("./conf.json"));
+
+import { register } from "module";
+//
+//  Modules
+//
+
+import { createRegisterHandler } from "./components/register/createRegisterHandler.js";
+
+//
+//  Code
+//
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
+app.use("/", express.static(path.join(__dirname, "public")));
+const server = http.createServer(app);
+const io = new Server(server);
+
+//
+//  Listeners
+//
+
+io.on('connection', (socket) => {
+    
+    //Register
+    const registerHandler  =  createRegisterHandler(socket);
+    registerHandler.registerReciver();
+
+    //
+
+
+});
+
+server.listen(conf.port, () => {
+  console.log("server running on port: " + conf.port);
+});
