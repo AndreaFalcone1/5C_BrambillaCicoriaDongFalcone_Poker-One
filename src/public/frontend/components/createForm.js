172f857fde0a63abb9c4f664f44e78d8332357fa
AndreaFalcone1 <falconeandrea@itis-molinari.eu>
2025-05-09 09:42:07 +0200

    Form, Index.js

--- src/public/frontend/components/createForm.js
+++ src/public/frontend/components/createForm.js
@@ -1,26 +1,94 @@
-const createForm = (parentElement) => {
+export const createForm = (parentElement) => {
+
     let type = '';
-    let submitCallback = () => {};
-    let cancelCallback = () => {};
+
+    const templateLogin = `
+        <div class="">    
+            <form class="">
+                <input type="text" id="loginEmail"/>
+                <input type="text" id="loginPassword"/>
+                <button type="button" id="loginSubmit">Submit</button>
+                <button type="button" id="loginCancel">Cancel</button>
+            </form>
+        </div>
+    `;
+
+    const templateRegister = `
+        <div class="">    
+            <form class="">
+                <input type="text" id="registerUsername"/>
+                <input type="text" id="registerEmail"/>
+                <input type="text" id="registerPassword"/>
+                <input type="text" id="registerName"/>
+                <input type="text" id="registerSurname"/>
+                <input type="date" id="registerDateOfBirth"/>
+                <button type="button" id="registerSubmit">Submit</button>
+                <button type="button" id="registerCancel">Cancel</button>
+            </form>
+        </div>
+    `;
+
+    let submitCallback, cancelCallback;
   
     return {
         onSubmit: (callbackFn) => { submitCallback = callbackFn; },
+
         onCancel: (callbackFn) => { cancelCallback = callbackFn; },
-        setType: (value) => { type = value; },
-        setStatus: (status) => { console.log("Status:", status); }, 
-        clear: () => { parentElement.innerHTML = ''; },
+        
         render: () => {
-            parentElement.innerHTML = `
-                <div>Type: ${type} <input type="text" id="typeInput" /></div>
-                <button id="submitBtn">Submit</button>
-                <button id="cancelBtn">Cancel</button>
-            `;
-        document.querySelector("#submitBtn").onclick = () => {
-            const val = document.querySelector("#typeInput").value;
-            submitCallback(val);
-        };
-        document.querySelector("#cancelBtn").onclick = cancelCallback;
-      },
-      build: (inputType) => { type = inputType; },
+
+            if (type.toLowerCase() == "login") {
+
+                //Use the login template
+                parentElement.innerHTML = templateLogin;
+
+                //Handle the submit
+                document.querySelector("#loginSubmit").onclick = () => {
+                    
+                    const dict = {};
+
+                    dict.email = document.querySelector("#loginEmail").value;
+                    dict.password = document.querySelector("#loginPassword").value;
+                    
+                    submitCallback(dict);
+                };
+
+                //Handle the reject
+                document.querySelector("#loginCancel").onclick = () => {
+                    parentElement.innerHTML = '';
+                    cancelCallback();
+                }
+                
+            } else if (type.toLowerCase() == "register") {
+
+                //Use the register template
+                parentElement.innerHTML = templateRegister;
+
+                //Handle the submit
+                document.querySelector("#registerSubmit").onclick = () => {
+                    
+                    const dict = {};
+                    
+                    dict.username = document.querySelector("#registerUsername").value;
+                    dict.email = document.querySelector('#registerEmail').value;
+                    dict.name = document.querySelector("#registerName").value;
+                    dict.surname = document.querySelector("#registerSurname").value;
+                    dict.date = document.querySelector("#registerDateOfBirth").value;
+                    
+                    submitCallback(dict);
+                };
+
+                //Handle the reject
+                document.querySelector("#registerCancel").onclick = () => {
+                    parentElement.innerHTML = '';
+                    cancelCallback();
+                }
+
+            } else {
+                return -1
+            }
+        },
+        
+        build: (inputType) => { type = inputType; }
     };
 };
--- src/public/index.js
+++ src/public/index.js
@@ -1,13 +1,16 @@
 import { createNavigator } from "./frontend/components/createNavigator.js";
 import { createRegisterHandler } from "./frontend/components/createRegisterHandler.js"
 import { createLoginHandler } from "./frontend/components/createLoginHandler.js"
+import { createForm } from "./frontend/components/createForm.js"
 
 //
 //  Binding
 //
 
-const login = document.getElementById("login");
-const register = document.getElementById("register");
+const loginButton = document.getElementById("login");
+const registerButton = document.getElementById("register");
+
+const divForm = document.getElementById("divFormLoginRegister");
 
 //
 //  Vars
@@ -18,8 +21,39 @@ const socket = io();
 const loginHandling = createLoginHandler(socket);
 const registerHandling = createRegisterHandler(socket);
 
-//console.log(registerHandling.registerSender('falconeandrea@itis-molinari.eu', 'xXSaraCinescaXx', 'Sara', 'Cinesca', '10/02/2000'));
-//registerHandling.registerReciver();
+const form = createForm(divForm);
 
-console.log(loginHandling.loginSender("falconeandrea@itis-molinari.eu", "galGdgj4AlLxG8z"));
-loginHandling.loginReciver();
+//
+//
+//
+
+loginButton.onclick = function() {
+    
+    form.build("login");
+
+    form.onSubmit(function (dict) {
+        console.log(loginHandling.loginSender(dict.email, dict.password));
+        loginHandling.loginReciver();
+    });
+
+    form.render();
+    
+}
+
+registerButton.onclick = function() {
+    
+    form.build("register");
+    
+    form.onSubmit(function (dict) {
+        console.log(registerHandling.registerSender(dict.email, dict.username, dict.name, dict.surname, dict.date));
+        registerHandling.registerReciver();
+    })  
+
+    form.render();
+
+}
+
+form.render();
+
+
+
